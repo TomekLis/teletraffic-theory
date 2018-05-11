2fa5e766fa0325b5e6b861eea0f6c69d55ac332e
tmkls <listomek307@gmail.com>
2018-05-11 23:02:35 +0200

    Scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ScrollSection, ScrollContainer } from './components/scroll';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <ScrollContainer>
+          <ScrollSection style={{
+            backgroundImage: 'url(http://www.thepetedesign.com/demos/phones.png)',
+            backgroundRepeat: 'no-repeat'
+          }}
+            pageId={0}>
+            <a href="https://github.com/DawnyWu/react-onepage-scroll">
+              <img style={{ position: 'absolute', top: 0, right: 0, border: 0, zIndex: 999 }} src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub" />
+            </a>
+          </ScrollSection>
+
+          <ScrollSection style={{
+            backgroundImage: 'url(http://www.thepetedesign.com/demos/back-phone.png)',
+            backgroundRepeat: 'no-repeat'
+          }}
+            pageId={1}>
+          </ScrollSection>
+
+          <ScrollSection style={{
+            backgroundImage: 'url(http://www.thepetedesign.com/demos/tilted-phone.png)',
+            backgroundRepeat: 'no-repeat'
+          }}
+            pageId={2}>
+          </ScrollSection>
+        </ScrollContainer>
+      </div>  
     );
   }
 }
